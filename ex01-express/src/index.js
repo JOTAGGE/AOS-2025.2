238ebd21dbc75fc401c24040cbf4ac3d669c9f3f
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2025-08-19 00:31:03 +0000

    index.js da atividade 1 atualizado

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Bem-Vindo ao Express de Gabriel!');
+app.get('/', (req, res) => {
+    res.send('Bem-Vindo ao Express de Gabriel!');
 });
 
-console.log(process.env.MY_SECRET);
+// Exporta o app para o Vercel cuidar
+module.exports = app;
